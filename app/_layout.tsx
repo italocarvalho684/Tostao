021d757fc944b6e3a5f60ac5ccb0bdc3b5cbbb77
Italo Carvalho <italocarvalho684@gmail.com>
2026-01-31 13:43:40 -0300

    feat: set up app foundations (i18n, theme, text, date locale)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,41 @@
-import { Stack } from "expo-router";
+import { initI18n } from "@/i18n";
+import { ThemeProvider } from "@/theme/context";
+import { customFontsToLoad } from "@/theme/typography";
+import { loadDateFnsLocale } from "@/utils/formatDate";
+import { useFonts } from "@expo-google-fonts/space-grotesk";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontError] = useFonts(customFontsToLoad)
+  const [isI18nInitialized, setIsI18nInitialized] = useState(false)
+
+  useEffect(() => {
+    initI18n()
+      .then(() => setIsI18nInitialized(true))
+      .then(() => loadDateFnsLocale())
+  }, [])
+
+  const loaded = fontsLoaded && isI18nInitialized;
+
+  useEffect(() => {
+    if (fontError) throw fontError
+  }, [fontError])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <ThemeProvider>
+        <Stack />
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
 }
